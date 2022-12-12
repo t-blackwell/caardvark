57e1ea4360b2162d2181d62fab6b022cdd5cd6e1
Steve Driffill <s.driffill@techmodal.com>
2022-12-12 16:03:28 +0000

    WIP: data model

--- prisma/migrations/20220224172159_init/migration.sql
+++ prisma/migrations/20220224172159_init/migration.sql
@@ -1,39 +1,31 @@
--- CreateTable
-CREATE TABLE "User" (
-    "id" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
+CREATE TABLE [user] (
+    user_id SERIAL NOT NULL,
+    email TEXT NOT NULL,
+    created_date TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_date TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+    CONSTRAINT pk_user PRIMARY KEY (user_id)
 );
 
--- CreateTable
-CREATE TABLE "Password" (
-    "hash" TEXT NOT NULL,
-    "userId" TEXT NOT NULL
+CREATE UNIQUE INDEX uk_user_email ON [user](email);
+
+CREATE TABLE [password] (
+    [hash] VARCHAR(100) NOT NULL,
+    user_id INTEGER NOT NULL,
+
+    CONSTRAINT fk_password_user_id FOREIGN KEY (user_id) REFERENCES [user](user_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
--- CreateTable
-CREATE TABLE "Note" (
-    "id" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "body" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-    "userId" TEXT NOT NULL,
+CREATE UNIQUE INDEX uk_password_user_id ON [password](user_id);
 
-    CONSTRAINT "Note_pkey" PRIMARY KEY ("id")
+CREATE TABLE note (
+    note_id SERIAL NOT NULL,
+    user_id INTEGER NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    body VARCHAR(MAX) NOT NULL,
+    created_date TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_date TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT pk_note PRIMARY KEY (note_id),
+    CONSTRAINT fk_note_user_id FOREIGN KEY (user_id) REFERENCES [user](user_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
-
--- CreateIndex
-CREATE UNIQUE INDEX "Password_userId_key" ON "Password"("userId");
-
--- AddForeignKey
-ALTER TABLE "Password" ADD CONSTRAINT "Password_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Note" ADD CONSTRAINT "Note_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
