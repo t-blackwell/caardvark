346c579b5a84decb4e921daf0cd29288a09dbae6
Aidan Saxon <a.saxon@techmodal.com>
2022-12-13 16:18:43 +0000

    updated to useSubmit and urlparam class

--- app/routes/cards/new.tsx
+++ app/routes/cards/new.tsx
@@ -1,16 +1,8 @@
-import {
-  FormControl,
-  InputLabel,
-  MenuItem,
-  NativeSelect,
-  Select,
-  TextField,
-} from "@mui/material";
+import { FormControl, InputLabel, Select } from "@mui/material";
 import type { ActionArgs, LoaderFunction } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { Form, useActionData, useLoaderData } from "@remix-run/react";
+import { Form, useLoaderData, useSubmit } from "@remix-run/react";
 import * as React from "react";
-import { requireUserId } from "~/session.server";
 import styles from "~/styles/cards/new.css";
 
 function getDummyData() {
@@ -45,35 +37,31 @@ function getDummyData() {
   };
 }
 
-type LoaderData = typeof getDummyData;
-
 export const loader: LoaderFunction = async ({ request, params }) => {
   const url = new URL(request.url);
   const searchParams = Object.fromEntries(url.searchParams.entries());
   console.log("serach paramas", searchParams);
   const dummyData = getDummyData();
-  const userId = await requireUserId(request);
 
   const filteredTemplates = dummyData.templates.filter(
     (template) =>
       searchParams.type === undefined ||
       template.type_id === Number(searchParams.type)
   );
-  return json({ types: dummyData.types, templates: filteredTemplates });
+  return json({
+    types: dummyData.types,
+    templates: filteredTemplates,
+    selectedType: searchParams.type,
+  });
 };
 
 export async function action({ request }: ActionArgs) {
-  console.log("DO action");
   const formData = await request.formData();
-  console.log(formData.entries(), " entries");
-  console.log(formData.get("whydoinotwork"), "  whydoinotwork");
-  if (formData.get("whydoinotwork") === "") {
-    console.log("i am an empty string");
+  const selectedType = formData.get("selectedType") || undefined;
+  if (typeof selectedType === "string") {
+    const searchParams = new URLSearchParams({ type: selectedType });
+    return redirect(`/cards/new?${searchParams}`);
   }
-  console.log(formData.get("hiddeninput"));
-
-  const type = formData.get("idonotexist");
-  console.log(type, " idonotexist");
 
   return redirect(`/cards/new`);
 }
@@ -83,13 +71,11 @@ export function links() {
 }
 
 export default function NewCardPage() {
-  const templateData = useLoaderData<LoaderData>();
-  const actionData = useActionData<typeof action>();
+  const templateData = useLoaderData<any>();
 
-  const selectForm = React.useRef<HTMLFormElement>(null);
-  const handleSubmit = () => {
-    console.log(selectForm.current);
-    selectForm.current?.submit();
+  const submit = useSubmit();
+  const handleSubmit = (event: any) => {
+    submit(event.currentTarget, { replace: true });
   };
 
   return (
@@ -101,43 +87,29 @@ export default function NewCardPage() {
         gap: 8,
         width: "100%",
       }}
-      ref={selectForm}
-      // onChange={handleSubmit}
+      onChange={handleSubmit}
     >
       <input type="hidden" name="hiddeninput" value={25} />
-      <TextField
-        id="select"
-        label="type"
-        defaultValue={""}
-        select
-        onChange={handleSubmit}
-        inputProps={{ name: "whydoinotwork" }}
-      >
-        {templateData.types.map((type) => (
-          <MenuItem key={type.id} value={type.id}>
-            {type.label}
-          </MenuItem>
-        ))}
-      </TextField>
-      {/* <FormControl fullWidth>
+      <FormControl fullWidth>
         <InputLabel id="demo-simple-select-label">Type</InputLabel>
-        
         <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
           label="Type"
-          inputProps={{ name: "type", type: "text" }}
-          defaultValue={""}
+          inputProps={{ name: "selectedType", type: "text" }}
+          value={templateData.selectedType}
+          native
         >
-          {templateData.types.map((type) => (
-            <MenuItem key={type.id} value={type.id}>
+          <option value={""}>All Templates</option>
+          {templateData.types.map((type: any) => (
+            <option key={type.id} value={type.id}>
               {type.label}
-            </MenuItem>
+            </option>
           ))}
         </Select>
-      </FormControl> */}
+      </FormControl>
       <div className="NewCard__templates">
-        {templateData.templates.map((template) => (
+        {templateData.templates.map((template: any) => (
           <div className="NewCard__template" key={template.id}>
             {template.text}
           </div>
