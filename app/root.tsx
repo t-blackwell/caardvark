c15f7347225b6240fe8b7c2bf94980f090e6255f
Constantin Diaconu <c.diaconu@techmodal.com>
2022-12-14 14:56:03 +0000

    remove navbar from login - fix

--- app/root.tsx
+++ app/root.tsx
@@ -14,7 +14,6 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-  useCatch,
   useLocation,
 } from "@remix-run/react";
 import * as React from "react";
@@ -44,8 +43,11 @@ interface DocumentProps {
 const Document = withEmotionCache(
   ({ children, title }: DocumentProps, emotionCache) => {
     const clientStyleData = React.useContext(ClientStyleContext);
-    const location = useLocation();
     const user = useOptionalUser();
+    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+      null
+    );
+    const location = useLocation();
     // Only executed on client
     useEnhancedEffect(() => {
       // re-link sheet container
@@ -78,11 +80,13 @@ const Document = withEmotionCache(
           />
         </head>
         <body>
-          {!["/signin", "/signup"].includes(location.pathname) ? (
+          {!["/login", "/signup"].includes(location.pathname) ? (
             <header>
-              {NavBar({
-                loggedIn: user !== undefined,
-              })}
+              <NavBar
+                loggedIn={user !== undefined}
+                anchorElUser={anchorElUser}
+                setAnchorElUser={setAnchorElUser}
+              />
             </header>
           ) : null}
           {children}
@@ -99,59 +103,6 @@ export default function App() {
   return (
     <Document>
       <Outlet />
-    </Document>
-  );
-}
-
-// https://remix.run/docs/en/v1/api/conventions#errorboundary
-export function ErrorBoundary({ error }: { error: Error }) {
-  console.error(error);
-
-  return (
-    <Document title="Error!">
-      <div>
-        <h1>There was an error</h1>
-        <p>{error.message}</p>
-        <hr />
-        <p>
-          Hey, developer, you should replace this with what you want your users
-          to see.
-        </p>
-      </div>
-    </Document>
-  );
-}
-
-// https://remix.run/docs/en/v1/api/conventions#catchboundary
-export function CatchBoundary() {
-  const caught = useCatch();
-
-  let message;
-  switch (caught.status) {
-    case 401:
-      message = (
-        <p>
-          Oops! Looks like you tried to visit a page that you do not have access
-          to.
-        </p>
-      );
-      break;
-    case 404:
-      message = (
-        <p>Oops! Looks like you tried to visit a page that does not exist.</p>
-      );
-      break;
-
-    default:
-      throw new Error(caught.data || caught.statusText);
-  }
-
-  return (
-    <Document title={`${caught.status} ${caught.statusText}`}>
-      <h1>
-        {caught.status}: {caught.statusText}
-      </h1>
-      {message}
     </Document>
   );
 }
