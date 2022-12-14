80abd1db3a269c2f9e699fee26a3cfd2ff55931d
Aidan Saxon <a.saxon@techmodal.com>
2022-12-14 15:06:13 +0000

    removed tailwind

--- app/routes/cards/index.tsx
+++ app/routes/cards/index.tsx
@@ -1,9 +1,8 @@
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { Form, Link, NavLink, useLoaderData } from "@remix-run/react";
+import { Link, NavLink, useLoaderData } from "@remix-run/react";
 import { getCardListItems } from "~/models/card.server";
 import { requireUserId } from "~/session.server";
-import { useUser } from "~/utils";
 
 export async function loader({ request }: LoaderArgs) {
   const userId = await requireUserId(request);
@@ -13,53 +12,29 @@ export async function loader({ request }: LoaderArgs) {
 
 export default function CardsPage() {
   const data = useLoaderData<typeof loader>();
-  const user = useUser();
 
   return (
-    <div className="flex h-full min-h-screen flex-col">
-      <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
-        <h1 className="text-3xl font-bold">
-          <Link to=".">Cards</Link>
-        </h1>
-        <p>{user.email}</p>
-        <Form action="/logout" method="post">
-          <button
-            type="submit"
-            className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
-          >
-            Logout
-          </button>
-        </Form>
-      </header>
-
-      <main className="flex h-full bg-white">
-        <div className="h-full w-80 border-r bg-gray-50">
-          <Link to="new" className="block p-4 text-xl text-blue-500">
-            + New Card
-          </Link>
-
-          <hr />
-
-          {data.cardListItems.length === 0 ? (
-            <p className="p-4">No cards yet</p>
-          ) : (
-            <ol>
-              {data.cardListItems.map((card) => (
-                <li key={card.card_id}>
-                  <NavLink
-                    className={({ isActive }) =>
-                      `block border-b p-4 text-xl ${isActive ? "bg-white" : ""}`
-                    }
-                    to={card.hash}
-                  >
-                    {`From "${card.from}" to "${card.to}"`}
-                  </NavLink>
-                </li>
-              ))}
-            </ol>
-          )}
-        </div>
-      </main>
+    <div>
+      <h1>
+        <Link to=".">Cards</Link>
+      </h1>
+      <div>
+        <Link to="new">+ New Card</Link>
+        <hr />
+        {data.cardListItems.length === 0 ? (
+          <p>No cards yet</p>
+        ) : (
+          <ol>
+            {data.cardListItems.map((card) => (
+              <li key={card.card_id}>
+                <NavLink to={card.hash}>
+                  {`From "${card.from}" to "${card.to}"`}
+                </NavLink>
+              </li>
+            ))}
+          </ol>
+        )}
+      </div>
     </div>
   );
 }
