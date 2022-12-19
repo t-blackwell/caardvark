d54e192c538d43acba57a4f4d50e2607ab6726a0
t-blackwell <t.blackwell@techmodal.com>
2022-12-19 11:41:42 +0000

    dropped font and color tables

--- app/models/message.server.ts
+++ app/models/message.server.ts
@@ -8,20 +8,20 @@ export function createMessage({
   text,
   card_id,
   from,
-  color_id,
-  font_id,
+  color,
+  font,
   image_url,
 }: Pick<
   message,
-  "text" | "card_id" | "from" | "color_id" | "font_id" | "image_url"
+  "text" | "card_id" | "from" | "color" | "font" | "image_url"
 >) {
   return prisma.message.create({
     data: {
       text,
       card_id,
       from,
-      color_id,
-      font_id,
+      color,
+      font,
       image_url,
     },
   });
@@ -37,35 +37,5 @@ export async function deleteMessage({
 
   return prisma.message.deleteMany({
     where: { message_id, card: { user_id: userId } },
-  });
-}
-
-export function getFonts() {
-  return prisma.font.findMany({
-    select: { font_id: true, name: true },
-  });
-}
-
-export function getFontByName(name: string) {
-  return prisma.font.findFirst({
-    select: {
-      font_id: true,
-    },
-    where: { name },
-  });
-}
-
-export function getColorByHex(hex: string) {
-  return prisma.color.findFirst({
-    select: {
-      color_id: true,
-    },
-    where: { hex: hex.toUpperCase() },
-  });
-}
-
-export function getColors() {
-  return prisma.color.findMany({
-    select: { color_id: true, name: true, hex: true },
   });
 }
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,8 +6,6 @@ const prisma = new PrismaClient();
 async function seed() {
   // cleanup the existing database
   await prisma.message.deleteMany();
-  await prisma.color.deleteMany();
-  await prisma.font.deleteMany();
   await prisma.card.deleteMany();
   await prisma.card_template.deleteMany();
   await prisma.card_type.deleteMany();
@@ -127,52 +125,13 @@ async function seed() {
     },
   });
 
-  const colorBlue = await prisma.color.create({
-    data: {
-      name: "blue",
-      hex: "#2962FF",
-    },
-  });
-
-  const colorRed = await prisma.color.create({
-    data: {
-      name: "red",
-      hex: "#FF0807",
-    },
-  });
-
-  const colorBlack = await prisma.color.create({
-    data: {
-      name: "black",
-      hex: "#000",
-    },
-  });
-
-  const fontOxygen = await prisma.font.create({
-    data: {
-      name: '"Oxygen"',
-    },
-  });
-
-  const fontArial = await prisma.font.create({
-    data: {
-      name: '"Arial"',
-    },
-  });
-
-  const fontHelvetica = await prisma.font.create({
-    data: {
-      name: '"Helvetica"',
-    },
-  });
-
   await prisma.message.create({
     data: {
       card_id: cardBirthday.card_id,
       from: "Little Bobby",
       text: "I love you grandma. You're the best friend a boy could ever have. Have a super day!",
-      color_id: colorBlue.color_id,
-      font_id: fontOxygen.font_id,
+      color: "#000",
+      font: "Arial",
       image_url:
         "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/1200px-Heart_coraz%C3%B3n.svg.png",
     },
@@ -183,8 +142,8 @@ async function seed() {
       card_id: cardBirthday.card_id,
       from: "Big Bobby",
       text: "Happy Birthday Ma! ",
-      color_id: colorBlack.color_id,
-      font_id: fontArial.font_id,
+      color: "#000",
+      font: "Arial",
       image_url:
         "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Gadus_morhua_Cod-2b-Atlanterhavsparken-Norway.JPG/495px-Gadus_morhua_Cod-2b-Atlanterhavsparken-Norway.JPG",
     },
@@ -195,8 +154,8 @@ async function seed() {
       card_id: cardBirthday.card_id,
       from: "Grandpa Joe",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam placerat ipsum felis, in rutrum diam iaculis vel. Quisque id tortor nec lorem consequat imperdiet. Nullam faucibus scelerisque tempus. Nullam suscipit lectus at nunc scelerisque viverra. Aliquam placerat imperdiet diam, id luctus velit ornare non. Pellentesque euismod risus id ligula consequat, id dictum quam volutpat. In eleifend est.",
-      color_id: colorRed.color_id,
-      font_id: fontHelvetica.font_id,
+      color: "#f00",
+      font: "Arial",
       image_url:
         "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Congrats_bqt.jpg/330px-Congrats_bqt.jpg",
     },
@@ -207,8 +166,8 @@ async function seed() {
       card_id: cardBirthday.card_id,
       from: "The Postman",
       text: "Knock knock",
-      color_id: colorBlue.color_id,
-      font_id: fontArial.font_id,
+      color: "#00f",
+      font: "Garamond",
     },
   });
 
