4ba842ace70e255e3a958b36bb61df7562711fd5
t-blackwell <t.blackwell@techmodal.com>
2022-12-20 17:07:09 +0000

    messages background

--- app/routes/$hash/index.tsx
+++ app/routes/$hash/index.tsx
@@ -164,7 +164,7 @@ export default function ViewCardPage() {
                 fullWidth
                 title="Add message"
                 to="new"
-                variant="outlined"
+                variant="contained"
               />
             </Container>
           ) : null}
